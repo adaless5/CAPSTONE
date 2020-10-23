541bc730963a12f7a030fcd54b32196c4dd8b4c2
NickGhezzi <54958462+NickGhezzi@users.noreply.github.com>
2020-10-22 23:05:39 -0400

    Started basic menus
    need to get it working with nav system

---
+++ Assets/Programming/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("exit");
+        Application.Quit();
+    }
+}
