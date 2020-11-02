73d6b77385c6728ae92ca81eccee99f9ecb384c5
Eric <ericmorrissette95@outlook.com>
2020-11-02 13:26:12 -0500

    NAVIGATION_MODULE 1.0
    New Features
    + Scene Connection System (seamless/portal) : Scenes can now be connected via Connectors
    + Scene Management window : Custom Scene management window added to facilitate the design process.
    + Multi Scene Editing : Scenes can now be edited simultaneously by adding the _s modifier to the levels name.
    
    Custom  Editors
    + Scene Connector Editor : Custom Editor with Color checks to make sure user has entered the data correctly.
    + Scene Manager Editor Window : Custom Editor window containing all scenes in the game, sorted by what level there in. Also contains checks showing the user what scenes are located in the build folder. Also contains information about all scene connectors found in the game. The window changes dynamically to show more or less information depending on its screen size. (expand the window to show more information)
    
    Prefabs
    + Connector : main prefab used for connecting scenes together.
       + Unload Trigger : used with seamless connectors as a trigger point to unload previous level.
       + Player Start : specifies player start point when exiting a connector
    + Spawn Point : place this in a level to have the player spawn at that location on game start.
    + Editor Light : lights are now bound to the player and don't unload on scene change. since player is spawned in on play there are no lights needed in a level. to facilitate editing this editor light provides lighting while editing a scene and is disabled on play.
    
    Database
    + Save System : A custom persistent data saving system wrapped over unity's built in player prefs. To allow for persistent data save between scene swaps and future play sessions.
    + Scene Connector Registry : This registry holds information about all the scene connectors found in the game. It is used to access connector data between multiple scenes as well as populate the scene manager editor window.

--- Assets/Programming/Scripts/NavigationSystem/PlayerStart.cs
+++ Assets/Programming/Scripts/NavigationSystem/PlayerStart.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//This class holds necessary information to spawn a player in a scene. 
+//also handles what connector the player spawns at while testing and editing a scene
 public class PlayerStart : MonoBehaviour
 {
     Object _playerPrefab;
@@ -9,49 +11,67 @@ public class PlayerStart : MonoBehaviour
     Vector3 _startRotation;
     Vector3 _halfCharacterControllerHeight = new Vector3(0,1.3f,0);
 
+    bool firstPass = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        //Load Player Prefab and initialize player
-        _playerPrefab = Resources.Load("Prefabs/Player/Player");
-        InitializePlayer();
-        //
+        firstPass = true;
+    }
 
-        //Creates a player at the start of the game.
-        if (GameObject.FindWithTag("Player") == null)
+    private void LateUpdate()
+    {
+        if (firstPass)
         {
-            Instantiate(_playerPrefab, _startPosition + _halfCharacterControllerHeight, Quaternion.Euler(_startRotation));
+            firstPass = false;
+
+            //Load Player Prefab and initialize player
+            _playerPrefab = Resources.Load("Prefabs/Player/Player");
+            InitializePlayer();
+            //
+
+            //Creates a player at the start of the game.
+            if (GameObject.FindWithTag("Player") == null)
+            {
+                _playerPrefab = Instantiate(_playerPrefab, _startPosition + _halfCharacterControllerHeight, Quaternion.Euler(_startRotation));
+
+                GameObject player = GameObject.FindWithTag("Player");
+                player.transform.position = _startPosition;
+                player.transform.Rotate(_startRotation, Space.Self);
+            }
+            //
         }
-        //
     }
 
     void OnDrawGizmos()
     {
         //Draw PlayerStart and direction coming out of scene Connector.
-        Gizmos.color = Color.red;
+        Gizmos.color = Color.blue;
         Gizmos.DrawSphere(transform.position, .5f);
         Gizmos.DrawLine(transform.position, transform.position + (transform.forward * 2));
         //
     }
 
-
     //This function runs when the game intially starts. It finds all active scene connectors in the scene then
-    //  finds the default and creates a player at that scene connectors position.
+    //finds the default and creates a player at that scene connectors position.
     void InitializePlayer()
     {
-        //Find Default SceneConnector to find level entry point.
-        SceneConnector[] sceneConnectors = Object.FindObjectsOfType<SceneConnector>();
-
-        foreach(SceneConnector connector in sceneConnectors)
+        
+        //Init Player From Spawn Point if its Present.
+        SpawnPoint spawnPoint = FindObjectOfType<SpawnPoint>();
+        if (spawnPoint != null)
         {
-            if (connector.isDefault)
-            {
-                //Get position and rotation of default player start for the scene.
-                _startPosition = connector.GetComponentInChildren<PlayerStart>().transform.position;
-                _startRotation = Vector3.zero + new Vector3(0,connector.GetComponentInChildren<PlayerStart>().transform.rotation.eulerAngles.y,0);
-                break;
-            }
+            _startPosition = spawnPoint.transform.position;
+            _startRotation = spawnPoint.transform.localEulerAngles;
+
         }
-        //
+        
+        //Else Init player from the first SceneConnector in the scene.
+        else
+        {
+            PlayerStart start = FindObjectOfType<SceneConnector>().GetComponentInChildren<PlayerStart>();
+            _startPosition = start.transform.position;
+            _startRotation = start.transform.localEulerAngles;
+        }
     }
 }
---
+++ Assets/Programming/Scripts/NavigationSystem/UnloadTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UnloadTrigger : MonoBehaviour
+{
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position,.5f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GetComponentInParent<SceneConnector>().SendMessage("UnloadScene");
+        }
+    }
+}
