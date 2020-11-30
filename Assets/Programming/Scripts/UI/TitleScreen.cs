1b7eee186fbcfb2c562529d340e09dd64b3509c9
NickGhezzi <54958462+NickGhezzi@users.noreply.github.com>
2020-11-30 16:27:32 -0500

    Title screen
    +new scene for title screen
    +cinematic cameras
    +menu assets
    +title screen script

---
+++ Assets/Programming/Scripts/UI/TitleScreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class TitleScreen : MonoBehaviour
+{
+    private void Update()
+    {
+        if(Input.anyKey)
+        {
+            SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+        }
+    }
+}
