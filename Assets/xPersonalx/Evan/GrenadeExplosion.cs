39a642d13cdaf5fdbd226746b524f1650fb9b1e2
Evan P <evanpersall@gmail.com>
2021-01-25 16:08:33 -0500

    Added shockwave and rubble particles
    Added shockwave behavior to grenade explosion script
    
    added material for shockwacce
    
    added rubble particle to grenade explosion prefab

--- Assets/xPersonalx/Evan/GrenadeExplosion.cs
+++ Assets/xPersonalx/Evan/GrenadeExplosion.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class GrenadeExplosion : MonoBehaviour
 {
 
-    public GameObject Shockwave;
+    public GameObject ImplosionWaveObject;
+    public GameObject[] ShockwaveObjects;
     Light ExplosionLight;
     float Lifetime;
 
@@ -14,6 +15,14 @@ public class GrenadeExplosion : MonoBehaviour
     {
         Lifetime = 4.0f;
         ExplosionLight = GetComponent<Light>();
+        for(int i = 0; i < ShockwaveObjects.Length;i++)
+        {
+            ShockwaveObjects[i].transform.localEulerAngles = new Vector3( Random.Range(-90.0f,90.0f), Random.Range(-90.0f, 90.0f),  0.0f);
+            if(i>0)
+            {
+                ShockwaveObjects[i].transform.localScale *= (float)(i + 1);
+            }
+        }
     }
 
     // Update is called once per frame
@@ -21,18 +30,50 @@ public class GrenadeExplosion : MonoBehaviour
     {
         Lifetime -= Time.deltaTime;
         ExplosionLight.intensity -= 100.0f;
-        if (Shockwave.transform.localScale.x > 0.0f)
+
+        Implosion();
+        Shockwave();
+
+        if (Lifetime < 0.0f)
+        {Destroy(gameObject);}
+
+
+    }
+    void Implosion()
+    {
+        if (ImplosionWaveObject.transform.localScale.x > 0.0f)
         {
-            Shockwave.transform.localScale -= new Vector3(1.0f, 1.0f, 1.0f);
+            ImplosionWaveObject.transform.localScale -= new Vector3(1.0f, 1.0f, 1.0f);
         }
         else 
         {
-            Shockwave.transform.localScale = new Vector3();
+            ImplosionWaveObject.transform.localScale = new Vector3();
         }
-        Shockwave.transform.Rotate(new Vector3(1.0f, 2.0f, 3.0f));
-        if (Lifetime < 0.0f)
+        ImplosionWaveObject.transform.Rotate(new Vector3(1.0f, 2.0f, 3.0f));
+    }
+    void Shockwave()
+    {
+        if(ShockwaveObjects[0] !=null)
         {
-            Destroy(gameObject);
+
+            foreach (GameObject shockwave in ShockwaveObjects)
+            {
+                shockwave.transform.localScale *= 1.5f;
+
+                Material mat = shockwave.GetComponent<MeshRenderer>().material;
+                if (mat.color.a > 0.01f)
+                {
+                    Color newColor = mat.color;
+                    newColor.a -= Time.deltaTime * 4.0f;
+                    mat.color = newColor;
+                    shockwave.GetComponent<MeshRenderer>().material = mat;
+                }
+                else
+                {
+                    Destroy(shockwave);
+                }
+            }
+
         }
     }
 }
