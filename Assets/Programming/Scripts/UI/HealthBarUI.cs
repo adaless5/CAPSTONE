69520520579a248a3d8ffc43df15798b920da819
nesacr <54957718+nesacr@users.noreply.github.com>
2021-02-26 01:03:36 -0500

    Health/Armor Bug Fixes & ArmorBarUI update
    *************************************************************************
    Class: Armor
    Author: Leandro Carrigan
    Created: 10/2020
    Last Modified: Fall Term 2020
    Description: Class that handles dealing damage and regeneration to player’s armor stat as well as working with the health script to ensure damage is allocated correctly.
    Change Log
    *************-
    Date: 02/26/2021
    Author: Vanessa Rakoczy
    Verified By: Leandro Carrigan
    Changes: Replaced Armor Bar UI references to use new radial slider logic instead of old scaling.
    **************************************************************************
    Class: ArmorBarUI
    Author: Vanessa Rakoczy
    Created: 10/2020
    Last Modified: Fall Term 2020
    Description: Class that handles logic to display and update the armor bar UI from the player’s armor stat.
    Change Log
    *************-
    Date: 02/26/2021
    Author: Vanessa Rakoczy
    Verified By: Leandro Carrigan
    Changes: Replaced Armor Bar UI references to use new radial slider logic instead of old scaling.
    **************************************************************************
    Class: HealthBarUI
    Author: Vanessa Rakoczy
    Created: 09/2020
    Last Modified: Fall Term 2020
    Description: Class that handles logic to display and update the health bar UI from the player’s health stat.
    Change Log
    *************-
    Date: 02/26/2021
    Author: Vanessa Rakoczy
    Verified By: Leandro Carrigan
    Changes: Updated logic on taking damage and healing to fix scaling bug where health scales negatively and past restricted bounds.
    **************************************************************************
    Asset: Player Prefab
    Author: N/A
    Created: 09/2020
    Last Modified: 02/18/2021
    Description: Prefab that handles all player logic including camera, weapon/tool equipment, HUD and menus.
    Change Log
    *************-
    Date: 01/28/2021
    Author: Vanessa Rakoczy
    Verified By: Leandro Carrigan
    Changes: Updated HUD overlay, rearranged UI elements to match with HUD overlay and updated Ammo Count UI to work with TextMeshPro.
    Date: 02/08/2021
    Author: Vanessa Rakoczy
    Verified By: Leandro Carrigan
    Changes: Added Compass element to HUD overlay
    Date: 02/11/2021
    Author: Vanessa Rakoczy
    Verified By: Anthony D & Leandro Carrigan
    Changes: Updated HUD overlay and UI completely to scale properly with free aspect versus 16:9.
    Date: 02/12/2021
    Author: Vanessa Rakoczy
    Verified By: Leandro Carrigan
    Changes: Added TextMeshPro and Image to HUD to work with interactable game objects.
    Date: 02/18/2021
    Author: Vanessa Rakoczy
    Verified By: Leandro Carrigan
    Changes: Updated Pause menu in player HUD to include “Controls” menu.
    Date: 02/26/2021
    Author: Vanessa Rakoczy
    Verified By: Leandro Carrigan
    Changes: Updated PlayerHUD armor bar to fill radially instead of scaling for readability.
    **************************************************************************

--- Assets/Programming/Scripts/UI/HealthBarUI.cs
+++ Assets/Programming/Scripts/UI/HealthBarUI.cs
@@ -10,7 +10,8 @@ public class HealthBarUI : MonoBehaviour
 
     private void Awake()
     {
-        // slider = GetComponent<Slider>();        
+        // slider = GetComponent<Slider>();  
+        
     }
 
     public void SetMaxHealth(float HP)
@@ -22,13 +23,29 @@ public class HealthBarUI : MonoBehaviour
     public void LoseHealth(float HP, float damage, float maxHP)
     {
         float damagePercent = damage / maxHP;
-        healthFill.transform.localScale -= new Vector3(damagePercent, damagePercent, damagePercent);        
+        
+        if (healthFill.transform.localScale.x <= 1 && healthFill.transform.localScale.x > 0)
+        {            
+
+            healthFill.transform.localScale -= new Vector3(damagePercent, damagePercent, damagePercent);
+
+            if(healthFill.transform.localScale.x < 0)
+            healthFill.transform.localScale = new Vector3(0f, 0f, 0f);
+                  
+        }
+        else
+        {
+            healthFill.transform.localScale = new Vector3(0f, 0f, 0f);
+        }
        // slider.value = HP;
     } 
 
     public void GainHealth(float HP, float healAmount, float maxHP)
     {
         float healPercent = healAmount / maxHP;
-        healthFill.transform.localScale += new Vector3(healPercent, healPercent, healPercent);
+
+        if(healthFill.transform.localScale.x < 1 && healthFill.transform.localScale.x >= 0)      
+        healthFill.transform.localScale += new Vector3(healPercent, healPercent, healPercent);       
+        
     }
 }
