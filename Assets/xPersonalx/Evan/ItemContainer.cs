a19fb0f1952e6cec3a88b6c380d8037e2b4b579f
Evan P <evanpersall@gmail.com>
2021-03-08 18:57:29 -0500

    Bonus reading week stuff
    Added behavior to eyelight for when it is hit
    
    Fixed item container to check what player has before spawning items, and added a chance to spawn an enemy
    
    added leaper enemy prototype

--- Assets/xPersonalx/Evan/ItemContainer.cs
+++ Assets/xPersonalx/Evan/ItemContainer.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 public class ItemContainer : MonoBehaviour
 {
     public GameObject _Container;
-    public GameObject[] _PossiblePickups;
+
+
+    public GameObject _DefaultAmmoPickup;
+    public GameObject _GrenadePickup;
+    public GameObject _CreaturePickup;
+    public GameObject _HealthPickup;
+    public GameObject _Jumper;
+
+
     string[] Tags = { "Creature_Weapon", "Player_Weapon", "Player_Blade", "Player_Mine" };
+    List<string> PossiblePickupsTags = new List<string>();
 
     bool bIsBroken;
     // Start is called before the first frame update
@@ -20,7 +29,78 @@ public class ItemContainer : MonoBehaviour
     {
 
     }
+    void CheckPlayerEquipment()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player)
+        {
+            WeaponBase playerWeapon = player.GetComponentInChildren<WeaponBase>();
+            if (playerWeapon.bIsObtained)
+            {
+                PossiblePickupsTags.Add("Player_Weapon");
+            }
+            CreatureWeapon creatureWeapon = player.GetComponentInChildren<CreatureWeapon>();
+            if (creatureWeapon.bIsObtained)
+            {
+                PossiblePickupsTags.Add("Creature_Weapon");
 
+            }
+            MineSpawner mineSpawner = player.GetComponentInChildren<MineSpawner>();
+            if (mineSpawner.bIsObtained)
+            {
+
+                PossiblePickupsTags.Add("Player_Mine");
+
+            }
+            Health health = player.GetComponentInChildren<Health>();
+            if (!health.IsAtFullHealth())
+            {
+                PossiblePickupsTags.Add("Health_Pickup");
+            }
+        }
+    }
+    void SpawnPickup()
+    {
+        CheckPlayerEquipment();
+        AmmoController ammoController = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<AmmoController>();
+
+        int randint = Random.Range(0, PossiblePickupsTags.Count);
+
+        if (PossiblePickupsTags[randint] == "Player_Weapon"/* && !ammoController.IsAmmoFullOfType("Player_Weapon")*/ )
+        {
+            GameObject pickup = Instantiate(_DefaultAmmoPickup, transform.position, transform.rotation, transform);
+            pickup.isStatic = false;
+            pickup.SetActive(true);
+        }
+
+        else if (PossiblePickupsTags[randint] == "Player_Mine"/* && !ammoController.IsAmmoFullOfType("Player_Mine")*/)
+        {
+            GameObject pickup = Instantiate(_GrenadePickup, transform.position, transform.rotation, transform);
+            pickup.isStatic = false;
+            pickup.SetActive(true);
+        }
+
+        else if (PossiblePickupsTags[randint] == "Creature_Weapon"/* && !ammoController.IsAmmoFullOfType("Creature_Weapon")*/)
+        {
+            GameObject pickup = Instantiate(_CreaturePickup, transform.position, transform.rotation, transform);
+            pickup.isStatic = false;
+            pickup.SetActive(true);
+        }
+
+        else if (PossiblePickupsTags[randint] == "Health_Pickup")
+        {
+            GameObject pickup = Instantiate(_HealthPickup, transform.position, transform.rotation, transform);
+            pickup.isStatic = false;
+            pickup.SetActive(true);
+        }
+
+        else
+        {
+            GameObject pickup = Instantiate(_Jumper, transform.position, transform.rotation, transform);
+            pickup.isStatic = false;
+            pickup.SetActive(true);
+        }
+    }
     public void Break(string tag)
     {
         if (!bIsBroken)
@@ -30,11 +110,7 @@ public class ItemContainer : MonoBehaviour
                 {
                     GetComponent<Collider>().enabled = false;
                     _Container.SetActive(false);
-
-                    int randint = Random.Range(0, _PossiblePickups.Length);
-                    GameObject pickup = Instantiate(_PossiblePickups[randint], transform.position, transform.rotation, transform);
-                    pickup.isStatic = false;
-                    pickup.SetActive(true);
+                    SpawnPickup();
                     bIsBroken = true;
                 }
             }
---
+++ Assets/xPersonalx/Evan/LeapingEnemyProjectile.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeapingEnemyProjectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float _ProjectileDamage;
+    public ParticleSystem _splatParticle;
+    public Rigidbody _rigidBody;
+    Collider _hitCollider;
+    public float _splatTime;
+    float _currentSplatTime;
+    public float _FullLifeTimer;
+    float _LifeTimer;
+    bool _bIsSplatting;
+    void Start()
+    {
+        _LifeTimer = _FullLifeTimer;
+        _bIsSplatting = false;
+        _rigidBody = gameObject.GetComponent<Rigidbody>();
+        _hitCollider = gameObject.GetComponent<Collider>();
+        _splatParticle.Stop();
+        _currentSplatTime = _splatTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.transform.parent != null)
+        {
+            if (collision.gameObject.transform.parent.tag == "Player")
+            {
+
+                collision.gameObject.transform.parent.GetComponent<ALTPlayerController>().CallOnTakeDamage(_ProjectileDamage);
+
+            }
+        }
+        _bIsSplatting = true;
+        _splatParticle.transform.position = transform.position;
+       // Debug.Log(collision.gameObject.transform.name);
+    }
+
+    void Splat()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+        _rigidBody.velocity = new Vector3();
+        _rigidBody.angularVelocity = new Vector3();
+        if (!_splatParticle.isPlaying)
+        { _splatParticle.Play(); }
+
+
+        if(_currentSplatTime > 0.0f)
+        {
+            _currentSplatTime -= Time.deltaTime;
+        }
+
+        else
+        {
+            _currentSplatTime = _splatTime;
+            _bIsSplatting = false;
+            gameObject.SetActive(false);
+        }
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(_bIsSplatting)
+        {
+            Splat();
+        }
+        if(gameObject.activeSelf == true)
+        {
+            if (_LifeTimer > 0.0f)
+            {
+                _LifeTimer -= Time.deltaTime;
+            }
+            else
+            {
+                _bIsSplatting = true;
+            }
+        }
+    }
+    public void Fire()
+    {
+
+        _currentSplatTime = _splatTime;
+        _bIsSplatting = false;
+        GetComponent<MeshRenderer>().enabled = true;
+        _LifeTimer = _FullLifeTimer;
+    }
+}
