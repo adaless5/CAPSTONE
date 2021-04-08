4333c43bccc0c0d1eba82e4473ab1d37a79e055e
Evan P <evanpersall@gmail.com>
2021-04-07 21:30:03 -0400

    Scene changes

--- Assets/xPersonalx/Anthony/Scripts/ThermalEquipment.cs
+++ Assets/xPersonalx/Anthony/Scripts/ThermalEquipment.cs
@@ -8,6 +8,8 @@ public class ThermalEquipment : Equipment
     ALTPlayerController _playerController;
     bool bIsInThermalView = false;
     Light _directionalLight;
+    List<Light> _SceneLights;
+    List<Color> _SceneLightColors;
 
     Vector4 _originalLightValues; ///evan added this
 
@@ -44,6 +46,31 @@ public class ThermalEquipment : Equipment
     {
         LoadDataOnSceneEnter();
     }
+
+    void SetLightsThermal()
+    {
+        _SceneLights = new List<Light>();
+        _SceneLightColors = new List<Color>();
+        Light[] lights = FindObjectsOfType<Light>();
+        for (int i = 0; i < lights.Length; i++)
+        {
+            if (lights[i].gameObject.tag != "Dir Light")
+            {
+                _SceneLights.Add(lights[i]);
+                _SceneLightColors.Add(lights[i].color);
+                lights[i].color = new Color(lights[i].color.r * 2, 0, lights[i].color.b * 2);
+            }
+        }
+    }
+
+    void SetLightsNormal()
+    {
+        for (int i = 0; i < _SceneLights.Count; i++)
+        {
+            _SceneLights[i].color = _SceneLightColors[i];
+        }
+    }
+
     // Update is called once per frame
     public override void Update()
     {
@@ -67,6 +94,8 @@ public class ThermalEquipment : Equipment
                 {
                     _particleSystemPrefab.transform.position = _playerController.gameObject.transform.forward * 10.0f;
                 }
+
+                CheckNewScene();
             }
         }
     }
@@ -76,51 +105,84 @@ public class ThermalEquipment : Equipment
         return bIsInThermalView;
     }
 
+    void ThermalOn()
+    {
+        ThermalSkin[] ThermalObjs = FindObjectsOfType<ThermalSkin>();
+        SetLightsThermal();
+        foreach (ThermalSkin obj in ThermalObjs)
+        {
+            obj.ChangeToThermalSkin();
+        }
+
+        if (_directionalLight != null)
+        {
+            _directionalLight.color = new Vector4(0.0f, 0.0f, 1.0f, 1.0f);
+        }
+
+        bIsInThermalView = true;
+    }
+    void ThermalOff()
+    {
+        ThermalSkin[] ThermalObjs = FindObjectsOfType<ThermalSkin>();
+        SetLightsNormal();
+        foreach (ThermalSkin obj in ThermalObjs)
+        {
+            obj.ChangeToNormalSkin();
+        }
+
+        if (_directionalLight != null)
+        {
+            if (_playerController.GetDarknessVolume())
+            {
+                _directionalLight.color = new Vector4(0.0f, 0.0f, 0.0f, 1.0f);
+            }
+            else if (!_playerController.GetDarknessVolume())
+            {
+                _directionalLight.color = _originalLightValues;
+            }
+        }
+
+        //_particleSystemPrefab.SetActive(false);
+        bIsInThermalView = false;
+    }
+
     public override void UseTool()
     {
-
-        ThermalSkin[] ThermalObjs = FindObjectsOfType<ThermalSkin>();
-
         if (!bIsInThermalView)
         {
-            foreach (ThermalSkin obj in ThermalObjs)
-            {
-                obj.ChangeToThermalSkin();
-            }
-
-            if (_directionalLight != null)
-            {
-                _directionalLight.color = new Vector4(0.0f, 0.0f, 1.0f, 1.0f);
-            }
-
-            bIsInThermalView = true;
-
+            ThermalOn();
         }
         else if (bIsInThermalView)
         {
-            foreach (ThermalSkin obj in ThermalObjs)
-            {
-                obj.ChangeToNormalSkin();
-            }
-
-            if (_directionalLight != null)
-            {
-                if (_playerController.GetDarknessVolume())
-                {
-                    _directionalLight.color = new Vector4(0.0f, 0.0f, 0.0f, 1.0f);
-                }
-                else if (!_playerController.GetDarknessVolume())
-                {
-                    _directionalLight.color = _originalLightValues;
-                }
-            }
-
-            //_particleSystemPrefab.SetActive(false);
-            bIsInThermalView = false;
+            ThermalOff();
         }
         _playerController.SetThermalView(bIsInThermalView);
     }
     /// EVAN ADDED ^^^
+
+
+    void CheckNewScene()
+    {
+        bool didswitch = false;
+
+        if (_SceneLights.Count != FindObjectsOfType<Light>().Length -1)
+        {
+            didswitch = true;
+        }
+
+        if (didswitch)
+        {
+            for (int i = 0; i < _SceneLightColors.Count; i++)
+            {
+                if (_SceneLights[i] != null)
+                {
+                    _SceneLights[i].color = _SceneLightColors[i];
+                }
+            }
+            ThermalOn();
+        }
+    }
+
 
     /// OG CODE VVVV
 
