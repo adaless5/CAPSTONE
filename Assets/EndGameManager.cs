84bfcca1afcfc28ce74c1ac858b731f9557fef90
NickGhezzi <54958462+NickGhezzi@users.noreply.github.com>
2021-04-12 18:09:19 -0400

    Added to Cutscene

--- Assets/EndGameManager.cs
+++ Assets/EndGameManager.cs
@@ -14,7 +14,6 @@ public class EndGameManager : MonoBehaviour
         endGameDirector = GetComponent<PlayableDirector>();
         EventBroker.OnPlayerSpawned += PlayerSpawn;
 
-
     }
 
     void PlayEndGameCutscene()
