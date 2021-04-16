ab7a74c8ca34fca495dee270220b660bd516638a
Morr0669 <54951081+Morr0669@users.noreply.github.com>
2021-04-16 08:48:48 -0400

    quicksave5

--- Assets/Programming/Scripts/AudioSystem/AudioManagers/Enemy/AudioManager_Roamer.cs
+++ Assets/Programming/Scripts/AudioSystem/AudioManagers/Enemy/AudioManager_Roamer.cs
@@ -2,17 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager_Roamer : MonoBehaviour
+public class AudioManager_Roamer : AudioManager
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioSource _audioSourceAttack;
+    public AudioSource _audioSourceDie;
+    public AudioSource _audioSourceHit;
+    public AudioSource _audioSourceStep;
+
+    //Attack Sounds
+    public AudioClip[] _bladeSwings;
+
+    //Die Sounds
+    public AudioClip[] _dieSounds;
+
+    //Step Sounds
+    public AudioClip[] _stepSounds;
+
+    //Hit Sounds
+    public AudioClip[] _hitSounds;
+
+
+    public override void Initialize()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TriggerStep()
     {
-        
+        //Randomize volume and pitch slightly
+        float randVolumeScale = Random.Range(0.8f, 1.2f);
+        float randomPitchScale = Random.Range(-0.3f, 0.3f);
+        _audioSourceStep.pitch += randomPitchScale;
+
+        //Play Sound
+        int index = Random.Range(0, _stepSounds.Length);
+        _audioSourceStep.PlayOneShot(_stepSounds[index], randVolumeScale);
+
+        //Reset initial pitch
+        _audioSourceStep.pitch = 1f;
+    }
+
+    public void TriggerHit()
+    {
+        //Randomize volume and pitch slightly
+        float randVolumeScale = Random.Range(0.8f, 1.2f);
+        float randomPitchScale = Random.Range(-0.3f, 0.3f);
+        _audioSourceHit.pitch += randomPitchScale;
+
+        //Play Sound
+        int index = Random.Range(0, _hitSounds.Length);
+        _audioSourceHit.PlayOneShot(_hitSounds[index], randVolumeScale);
+
+        //Reset initial pitch
+        _audioSourceHit.pitch = 1f;
+    }
+
+    public void TriggerAttack()
+    {
+        //Randomize volume and pitch slightly
+        float randVolumeScale = Random.Range(0.8f, 1.2f);
+        float randomPitchScale = Random.Range(-0.3f, 0.3f);
+        _audioSourceAttack.pitch += randomPitchScale;
+
+        //Play Sound
+        int index = Random.Range(0, _bladeSwings.Length);
+        _audioSourceAttack.PlayOneShot(_bladeSwings[index], randVolumeScale);
+
+        //Reset initial pitch
+        _audioSourceAttack.pitch = .8f;
+    }
+
+    public void TriggerDeath()
+    {
+
     }
 }
