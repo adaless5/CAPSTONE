1a214ea69169a2bff5aa689accd27ab55cee43d6
Morr0669 <54951081+Morr0669@users.noreply.github.com>
2021-04-16 10:35:28 -0400

    Final Sound Part 1 of 2
    Added and implemented lots of new sounds.
    
    + Blade Sounds
    + Goggle Sounds
    + Pickup Ammo Sounds
    + Portal Sounds
    + Switch sounds
    + Door Sounds
    + Obelisk Sounds
    + Teleporter Enemy Sounds Part 1
    + Roamer Sounds
    + Break Container Sounds
    + Break goo sounds
    + Final Soundtrack Song

--- Assets/Programming/Scripts/AudioSystem/AudioManagers/Enemy/AudioManager_Teleporter.cs
+++ Assets/Programming/Scripts/AudioSystem/AudioManagers/Enemy/AudioManager_Teleporter.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager_Teleporter : MonoBehaviour
+public class AudioManager_Teleporter : AudioManager
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioSource _audioSourceTeleport;
+
+
+    public override void Initialize()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TriggerTeleport()
     {
-        
+        //Randomize volume and pitch slightly
+        float randVolumeScale = Random.Range(0.8f, 1.2f);
+        float randomPitchScale = Random.Range(-0.3f, 0.3f);
+        _audioSourceTeleport.pitch += randomPitchScale;
+
+        //Play Sound
+        _audioSourceTeleport.PlayOneShot(_audioSourceTeleport.clip, randVolumeScale);
+
+        //Reset initial pitch
+        _audioSourceTeleport.pitch = 1.44f;
     }
+
+    public bool teleportIsPlaying()
+    {
+        return _audioSourceTeleport.isPlaying;
+    }
+
+
 }
