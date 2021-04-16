2f720e15a1a192d5e720bde8f7ddeec0a0cd24af
adaless5 <43971175+adaless5@users.noreply.github.com>
2021-04-16 13:27:59 -0400

    Removing potential debug

--- Assets/EndGameManager.cs
+++ Assets/EndGameManager.cs
@@ -42,7 +42,7 @@ public class EndGameManager : MonoBehaviour
     {
         if (ALTPlayerController.instance.CheckForInteract())
         {
-            EventBroker.CallOnGameEnd();
+            //EventBroker.CallOnGameEnd();
             //PlayEndGameCutscene();
         }
     }
