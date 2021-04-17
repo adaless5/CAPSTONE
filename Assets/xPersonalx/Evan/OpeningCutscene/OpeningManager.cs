48f22a69e7b9fefee088b9ef6335058eb5945f48
LeandroCCplusplus <54957731+LeandroCCplusplus@users.noreply.github.com>
2021-04-16 23:08:39 -0400

    ...Forgot the proper delay before being able to continue

--- Assets/xPersonalx/Evan/OpeningCutscene/OpeningManager.cs
+++ Assets/xPersonalx/Evan/OpeningCutscene/OpeningManager.cs
@@ -21,7 +21,7 @@ public class OpeningManager : MonoBehaviour
     bool canStart = false;
     IEnumerator Start()
     {
-        new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(3.0f);
         canStart = true;
     }
 
